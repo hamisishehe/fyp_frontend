24e63c13ebe4b287bc2ddeadbca48410dae9f309
hamisi <hamisishehe@gmail.com>
2025-03-25 12:42:47 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
   title = 'frontend';
 }
